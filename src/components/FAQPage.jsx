7df5d2b36ccc00ba3799ed009c2caf3f00f9da13
gyanendra19 <gyanendrav416@gmail.com>
2024-06-19 17:05:09 +0530

    added faq section

---
+++ src/components/FAQPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FAQs from './FAQs'
+
+const RetailPage = () => {
+    return (
+        <section className='w-full h-screen flex items-center'>
+            <div className='w-3/4 h-[80%] ml-auto flex flex-col gap-5 mr-10 p-10 shadow-2xl overflow-y-scroll'>
+               <FAQs />
+            </div>
+        </section>
+    )
+}
+
+export default RetailPage
---
+++ src/components/FAQs.jsx
@@ -0,0 +1,65 @@
+import React, { Fragment, useEffect, useState } from 'react'
+import { RiArrowRightSLine } from '@remixicon/react'
+import { onValue, ref, set } from 'firebase/database'
+import { database } from '../utils/firebaseConfig'
+
+
+const FAQs = ({ data }) => {
+    const [faq, setFaq] = useState([])
+    const [showSection, setShowSection] = useState(false)
+
+    useEffect(() => {
+        onValue(ref(database, 'data/FAQPage/faqBox'), (snapshot) => {
+            if (snapshot !== null) {
+                setFaq(snapshot.val())
+            }
+        })
+    }, [])
+
+
+    // const updateText = (update, selector) => {
+    //     selector === 'title' && setRetailMain(prev => ({...prev, title: update}))
+    //     selector === 'image' && setRetailMain(prev => ({...prev, image: update}))
+    //     selector === 'para' && setRetailMain(prev => ({...prev, para: update}))
+    //     selector === 'bg' && setRetailMain(prev => ({...prev, bg: update}))
+    // }
+
+    const writeUserData = () => {
+        let success = false
+        set(ref(database, 'data/retailPage/genieBox'), faq);
+        success = true
+        if (success) alert('Date Changed')
+    }
+
+
+    return (
+        <Fragment>
+            <h1 onClick={() => setShowSection(prev => !prev)} className='text-xl font-medium flex gap-1 items-center'>FAQ's <span><RiArrowRightSLine /></span></h1>
+            <div className={`${showSection ? '' : 'hidden'} `}>
+                {faq?.length !== 0 && faq.map((question) => (
+                    <>
+                        {Object.keys(question).length !== 0 && Object.keys(question).map(key => (
+                                <div className={`flex flex-col gap-1`}>
+                                    <div className='flex flex-col gap-2'>
+                                        <label className='font-medium mt-3' htmlFor={key}>{key}</label>
+                                        <input
+                                            // onChange={(e) => updateText(e.target.value, key)}
+                                            className='w-[80%] border border-gray-200 rounded-sm px-3 h-[40px] focus:outline-none'
+                                            type="text"
+                                            id={key}
+                                            value={question[key] || ''} />
+                                        {key.startsWith('image') && (
+                                            <img className='w-44' src={question[key]} alt="" />
+                                        )}
+                                    </div>
+                                </div> 
+                        ))}
+                    </>
+                ))}
+                <button className='mt-4 px-3 py-1 bg-blue-400 rounded-md font-medium' onClick={() => writeUserData()}>Change</button>
+            </div>
+        </Fragment>
+    )
+}
+
+export default FAQs
