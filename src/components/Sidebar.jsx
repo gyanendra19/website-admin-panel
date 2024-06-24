5b7a814bc32c0a0d681d659f2ce549735ba83b2f
gyanendra19 <gyanendrav416@gmail.com>
2024-06-25 00:16:22 +0530

    completed authentication

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import { useFirebaseContext } from '../contexts/FirebaseContext'
+import { signOut } from 'firebase/auth'
+import { auth } from '../utils/firebaseConfig'
+import { useNavigate } from 'react-router-dom'
 
 const Sidebar = () => {
+  const navigate = useNavigate()
+  const userlogout = () => {
+    signOut(auth)
+    localStorage.removeItem('token')
+    navigate('/login')
+  }
 
   const {setPage} = useFirebaseContext()
 
@@ -16,6 +24,7 @@ const Sidebar = () => {
             <span onClick={() => setPage('FAQPage')} className='tracking-wider cursor-pointer'>FAQ Page</span>
             <span onClick={() => setPage('ContactPage')} className='tracking-wider cursor-pointer'>Contact Page</span>
             <span onClick={() => setPage('AboutPage')} className='tracking-wider cursor-pointer'>About Us Page</span>
+            <button onClick={() => userlogout()} className='px-4 py-2 rounded-md font-medium bg-white mt-3 w-[100px] text-black'>Logout</button>
         </div>
    </section>
   )
